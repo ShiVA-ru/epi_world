ae9037ca659528a6f151a2ad64af5b6b6c14a6d5
Shinyaev Vladimir <secman#bk.ru>
2023-11-18 18:05:37 +0500

    add button and input components

---
+++ src/lib/components/forms/EWButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let buttonType: 'button' | 'submit' = 'button';
+</script>
+
+<button class="button {$$props.class}" type={buttonType} on:click>
+	<slot />
+</button>
+
+<style lang="scss">
+	.button {
+		padding: 7px 5px;
+		line-height: 1;
+		border: none;
+		border-radius: 5px;
+
+		&:focus-visible {
+			outline: none;
+		}
+	}
+</style>
