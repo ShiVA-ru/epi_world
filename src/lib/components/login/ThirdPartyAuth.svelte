b06a88f3490b15845c9226815f03bd6ad9532a84
Shinyaev Vladimir <secman#bk.ru>
2023-11-19 11:11:57 +0500

    add registration route

---
+++ src/lib/components/login/ThirdPartyAuth.svelte
@@ -0,0 +1,57 @@
+<div class="third-party-auth">
+	<p class="third-party-auth__title">Войти с помощью</p>
+	<div class="third-party-auth__links">
+		<a
+			href="google.com"
+			class="third-party-auth__link"
+		>
+			<img
+				src="/icons/google.svg"
+				alt="Лого google"
+			/>
+		</a>
+		<a
+			href="ya.ru"
+			class="third-party-auth__link"
+		>
+			<img
+				src="/icons/yandex.png"
+				alt="Лого yandex"
+			/>
+		</a>
+	</div>
+	<a
+		class="third-party-auth__forget-link"
+		href="/">Не удается зайти</a
+	>
+</div>
+
+<style lang="scss">
+	.third-party-auth {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 50px;
+
+		&__title {
+			// margin-bottom: 50px;
+			// text-align: center;
+			color: var(--light);
+			font-size: 16px;
+			font-weight: 400;
+		}
+
+		&__links {
+			display: flex;
+			gap: 45px;
+			align-items: center;
+		}
+
+		&__forget-link {
+			color: var(--light);
+			font-size: 16px;
+			font-weight: 400;
+		}
+	}
+</style>
